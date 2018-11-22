8da9223a7cbf4c3fbb50e645df96993c5a6a658d
edison cuadros <ediison@hotmail.com>
2018-11-22 11:00:32 -0500

    no message

---
+++ Presentation.Web/Controllers/PermisoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentation.Web.Controllers
+{
+    public class PermisoController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
