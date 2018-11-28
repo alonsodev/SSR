29ec1aa343c2200b87d2301639a7f2739ce8c9e2
edison cuadros <ediison@hotmail.com>
2018-11-28 09:47:44 -0500

    no message

---
+++ Infrastructure.Data/Repositories/InterestAreaRepository.cs
@@ -0,0 +1,18 @@
+﻿using EntityFramework.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class InterestAreaRepository : Repository<interest_areas>
+    {
+        internal InterestAreaRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+       
+    }
+}
---
+++ Infrastructure.Data/Repositories/InvestigationGroupRepository.cs
@@ -0,0 +1,18 @@
+﻿using EntityFramework.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class InvestigationGroupRepository : Repository<investigation_groups>
+    {
+        internal InvestigationGroupRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+       
+    }
+}
