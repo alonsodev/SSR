538b4cd23fd75b83b1d5ad99db8de2ac2ecefd7c
edison cuadros <ediison@hotmail.com>
2019-04-11 14:34:07 -0500

    V3

---
+++ Presentation.Web/Controllers/PoliticasUsoController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Logic;
+using iTextSharp.text;
+using Presentation.Web.Filters;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rectangle = iTextSharp.text.Rectangle;
+
+namespace Presentation.Web.Controllers
+{
+    public class PoliticasUsoController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            /*SendEmailNotificationBL oSendEmailNotificationBL = new SendEmailNotificationBL();
+            oSendEmailNotificationBL.EnviarNotificacionActivarCuenta();*/
+
+            ConfigurationBL oConfigurationBL = new ConfigurationBL();
+            ViewBag.terms_conditions = oConfigurationBL.Obtener().terms_conditions;
+
+            return View();
+        }
+
+     
+        
+    }
+}
